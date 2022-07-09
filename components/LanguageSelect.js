6f9450b457f03b23b81ab47ebcadfddf37127819
sreenivasulu.s097 <sreenivasulu.s097@gmail.com>
2022-07-09 14:10:39 +0530

    language dropdown functionality implemented

--- components/LanguageSelect.js
+++ components/LanguageSelect.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "next-i18next";
+import { useRouter } from "next/router";
 
 const languageMap = {
   en: { label: "English", dir: "ltr", active: true },
@@ -8,22 +9,38 @@ const languageMap = {
 };
 
 const LanguageSelect = () => {
-  const [langSelected, setLangSelected] = useState("en");
-  /*useEffect(() => {
-    setLangSelected(localStorage.getItem("i18nextLng") || "en");
-  }, []);*/
+  const router = useRouter();
+  const { locale } = router;
+  //const [langSelected, setLangSelected] = useState("en");
+  /* useEffect(() => {
+    setLangSelected(locale);
+    console.log("useeffect locale", locale);
+  }, [locale]); */
 
   const { t, i18n } = useTranslation();
-  console.log("i18n", i18n);
+  // console.log("i18n", i18n, useTranslation());
 
   const handleLanguageChange = (e) => {
+    // console.log("language change", e.target.value);
+    //setLangSelected(e.target.value);
     i18n.changeLanguage(e.target.value);
-    setLangSelected(e.target.value);
+
+    router.push(
+      {
+        route: router.pathname,
+        query: router.query,
+      },
+      router.asPath,
+      { locale: e.target.value },
+      { shallow: true }
+    );
+    //  router.push(`/${e.target.value}/${router.asPath}`);
+    //console.log("router on change", router);
   };
   return (
     <div className="w-[100%] text-sm   pl-2 border border-lognScrnBrderColorLight dark:border-bgLoginLayoutColor rounded-md">
       <select
-        defaultValue={langSelected}
+        defaultValue={locale}
         onChange={(e) => handleLanguageChange(e)}
         className="bg-white dark:bg-bgLoginLayoutColor "
       >
